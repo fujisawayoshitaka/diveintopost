d65be746079355b37018acb892c51987e27caf7d
fujisawayoshitaka <1317699@gmail.com>
2020-01-28 22:31:08 +0900

    アジェンダ削除をオーナーおよび作成者自身可能な形で実装、また削除時チームメンバーに対しメール送信を実装

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,5 +1,5 @@
 class AgendasController < ApplicationController
-  # before_action :set_agenda, only: %i[show edit update destroy]
+   before_action :set_agenda, only: %i[show edit update destroy]
 
   def index
     @agendas = Agenda.all
@@ -15,9 +15,25 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
+    end
+  end
+
+  def destroy
+    team_members = @agenda.team.members
+    agenda_user = @agenda.user_id
+    owner_id = @agenda.team.owner_id
+    if current_user.id != agenda_user && current_user.id != owner_id
+      redirect_to dashboard_url, notice: 'agendaの作成者とチームリーダーのみ削除できます。'
+    elsif @agenda.destroy
+      team_members.each do | member |
+        AssignMailer.delete_agenda_mail(member.email, @agenda.title).deliver
+      end
+      redirect_to dashboard_url, notice: 'agendaを削除しました。'
+    else
+      redirect_to dashboard_url, notice: 'agendaの削除に失敗しました。'
     end
   end
 
